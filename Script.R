1c6c2f7a0c0de26da54eee3ec868cafe2cc942cd
Guille Ferchero <guilleferchero@gmail.com>
2022-04-03 20:42:14 -0300

    limpieza y nuevas columnas

--- Script.R
+++ Script.R
@@ -1,2 +1,21 @@
-# limpiamos memoria
-jj
+#Limpiamos memoria
+
+rm(list = ls())
+
+#Instalamos Librerías
+
+#install.packages("DT")
+library("DT")
+library(tidyverse)
+library(lubridate)
+library(kableExtra)
+library(plotly)
+library("chessR")
+
+# Levantamos ultimos datos
+lichess_game_data <- get_raw_lichess("Guillebarracas")
+base <- write.csv(lichess_game_data, "base.csv")
+
+
+j
+
